de2c717ffbf698ee62e1333fd72bc577786557f4
Ben Sutherland <blender.sleuth@gmail.com>
2017-01-05 12:24:00 +1100

    Initial Commit

---
+++ Unlokit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unlokit
+//
+//  Created by Ben Sutherland on 5/1/17.
+//  Copyright © 2017 blendersleuthdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
